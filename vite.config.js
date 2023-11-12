687faf04c6cd53721a4029de70d352db13080349
Vitalii Sapiak <vsapiak5@gmail.com>
2023-11-12 23:07:42 +0100

    second commit

---
+++ vite.config.js
@@ -0,0 +1,24 @@
+import { defineConfig } from "vite";
+import { glob } from 'glob';
+import injectHTML from 'vite-plugin-html-inject';
+import FullReload from 'vite-plugin-full-reload';
+import image from '@rollup/plugin-image';
+
+export default defineConfig({
+    base: '/name/',
+
+    root: 'src',
+    build: {
+        rollupOptions: {
+            input: glob.sync('./src/*.html'),
+        },
+        outDir: '../dist',
+    },
+    input: 'src/index.js',
+        output: {
+            dir: 'output',
+            format: 'cjs'
+    },
+
+    plugins: [injectHTML(), FullReload(['./src/**/**.html']), image()],
+});
